d2dbc0007c08972a02bfaa3726ac275c7ee5558b
Walter Barbieri <walter90@hotmail.it>
2023-06-28 14:35:41 +0200

    Commit 3 - Esercizio 3 completato

--- Esercizio/src/Esercizio3/App3.java
+++ Esercizio/src/Esercizio3/App3.java
@@ -1,15 +1,21 @@
 package Esercizio3;
 
+import java.util.Scanner;
+
 public class App3 {
+	static String myString;
 
 	public static void main(String[] args) {
-		// do {
-		// Scanner myObj = new Scanner(System.in);
-		// System.out.println("Inserire una stringa: ");
-		//
-		// } while (condition);
-		String myString = "Ciao";
-		System.out.println(myString.split());
+		do {
+			Scanner myObj = new Scanner(System.in);
+			System.out.println("Inserire una stringa: ");
+			myString = myObj.nextLine();
+			String[] chars = myString.split("");
+			for (int i = 0; i < chars.length; i++) {
+				System.out.println(chars[i] + ",");
+			}
+
+		} while (!myString.equals(":q"));
 
 	}
 
